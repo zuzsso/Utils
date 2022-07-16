6886e69a0f18cfd67faf4982f97754a4fca0f83e
Nicky Santamaria <nicky.web.001@gmail.com>
2022-07-16 20:25:25 +0100

    Compare IpV4 operator

--- src/Networking/Object/IpV4.php
+++ src/Networking/Object/IpV4.php
@@ -6,50 +6,54 @@ namespace Utils\Networking\Object;
 
 use LogicException;
 
-class IpV4
-{
-	private int $firstOctet;
-	private int $secondOctet;
-	private int $thirdOctect;
-	private int $fourthOctet;
+class IpV4 {
+    private int $firstOctet;
+    private int $secondOctet;
+    private int $thirdOctect;
+    private int $fourthOctet;
 
-	private function __construct(int $firstOctet, int $secondOctet, int $thirdOctet, int $fourthOctet)
-	{
-		$this->firstOctet = $firstOctet;
-		$this->secondOctet = $secondOctet;
-		$this->thirdOctect = $thirdOctet;
-		$this->fourthOctet = $fourthOctet;
-	}
+    private function __construct(int $firstOctet, int $secondOctet, int $thirdOctet, int $fourthOctet) {
+        $this->firstOctet = $firstOctet;
+        $this->secondOctet = $secondOctet;
+        $this->thirdOctect = $thirdOctet;
+        $this->fourthOctet = $fourthOctet;
+    }
 
-	/**
-	 * @param string $ipInStringFormat
-	 * @return static
-	 */
-	public static function constructFromString(string $ipInStringFormat): self
-	{
-		$valid = filter_var($ipInStringFormat, FILTER_VALIDATE_IP);
+    /**
+     * @param string $ipInStringFormat
+     * @return static
+     */
+    public static function constructFromString(string $ipInStringFormat): self {
+        $valid = filter_var($ipInStringFormat, FILTER_VALIDATE_IP);
 
-		if ($valid === false) {
-			throw new LogicException("Unrecognized IP V4 address: '$ipInStringFormat'");
-		}
+        if ($valid === false) {
+            throw new LogicException("Unrecognized IP V4 address: '$ipInStringFormat'");
+        }
 
 
-		$parts = explode('.', $valid);
+        $parts = explode('.', $valid);
 
-		if (count($parts) !== 4) {
-			throw new LogicException("Expected to find four octets");
-		}
+        if (count($parts) !== 4) {
+            throw new LogicException("Expected to find four octets");
+        }
 
-		return new self (
-			(int)$parts[0],
-			(int)$parts[1],
-			(int)$parts[2],
-			(int)$parts[3],
-		);
-	}
+        return new self (
+            (int)$parts[0],
+            (int)$parts[1],
+            (int)$parts[2],
+            (int)$parts[3],
+        );
+    }
 
-	public function __toString(): string
-	{
-		return $this->firstOctet . '.' . $this->secondOctet . '.' . $this->thirdOctect . '.' . $this->fourthOctet;
-	}
+    public function equalsTo(IpV4 $anotherIpV4): bool {
+        return
+            ($this->firstOctet === $anotherIpV4->firstOctet) &&
+            ($this->secondOctet === $anotherIpV4->secondOctet) &&
+            ($this->thirdOctect === $anotherIpV4->thirdOctect) &&
+            ($this->fourthOctet === $anotherIpV4->fourthOctet);
+    }
+
+    public function __toString(): string {
+        return $this->firstOctet . '.' . $this->secondOctet . '.' . $this->thirdOctect . '.' . $this->fourthOctet;
+    }
 }
--- tests/Networking/Object/IpV4Test.php
+++ tests/Networking/Object/IpV4Test.php
@@ -8,64 +8,85 @@ use LogicException;
 use PHPUnit\Framework\TestCase;
 use Utils\Networking\Object\IpV4;
 
-class IpV4Test extends TestCase
-{
+class IpV4Test extends TestCase {
 
-	public function correctlyBuildsObjectDataProvider(): array
-	{
-		return [
-			['10.1.2.3'],
-			['255.255.255.255'],
-			['0.0.0.0'],
-			['127.0.0.1']
+    public function correctlyBuildsObjectDataProvider(): array {
+        return [
+            ['10.1.2.3'],
+            ['255.255.255.255'],
+            ['0.0.0.0'],
+            ['127.0.0.1']
+        ];
+    }
 
-		];
-	}
+    /**
+     * @dataProvider correctlyBuildsObjectDataProvider
+     * @param string $fixture
+     */
+    public function testCorrectlyBuildsObject(string $fixture): void {
+        $sut = IpV4::constructFromString($fixture);
 
-	/**
-	 * @dataProvider correctlyBuildsObjectDataProvider
-	 * @param string $fixture
-	 */
-	public function testCorrectlyBuildsObject(string $fixture): void
-	{
-		$sut = IpV4::constructFromString($fixture);
+        self::assertEquals($fixture, $sut->__toString());
+    }
 
-		self::assertEquals($fixture, $sut->__toString());
-	}
+    public function correctlyFailsBuildingObjectDataProvider(): array {
+        return [
+            ['', "Unrecognized IP V4 address: ''"],
+            ['test', "Unrecognized IP V4 address: 'test'"],
+            ['0', "Unrecognized IP V4 address: '0'"],
+            ['0.00.000.0000', "Unrecognized IP V4 address: '0.00.000.0000'"],
+            ['1.2.3.4.5', "Unrecognized IP V4 address: '1.2.3.4.5'"],
+            ['192,168,0,25', "Unrecognized IP V4 address: '192,168,0,25'"],
+            ['127. 0. 0. 1', "Unrecognized IP V4 address: '127. 0. 0. 1'"],
+            ['-125.0.0.1', "Unrecognized IP V4 address: '-125.0.0.1'"],
+            ['256.0.0.1', "Unrecognized IP V4 address: '256.0.0.1'"],
+            ['0.-255.0.1', "Unrecognized IP V4 address: '0.-255.0.1'"],
+            ['0.256.0.1', "Unrecognized IP V4 address: '0.256.0.1'"],
+            ['0.0.-3.1', "Unrecognized IP V4 address: '0.0.-3.1'"],
+            ['0.0.256.1', "Unrecognized IP V4 address: '0.0.256.1'"],
+            ['0.0.0.-3', "Unrecognized IP V4 address: '0.0.0.-3'"],
+            ['0.0.0.256', "Unrecognized IP V4 address: '0.0.0.256'"],
+            ['1a.0.0.255', "Unrecognized IP V4 address: '1a.0.0.255'"],
+            ['a1.0.0.255', "Unrecognized IP V4 address: 'a1.0.0.255'"],
+        ];
+    }
 
-	public function correctlyFailsBuildingObjectDataProvider(): array
-	{
-		return [
-			['', "Unrecognized IP V4 address: ''"],
-			['test', "Unrecognized IP V4 address: 'test'"],
-			['0', "Unrecognized IP V4 address: '0'"],
-			['0.00.000.0000', "Unrecognized IP V4 address: '0.00.000.0000'"],
-			['1.2.3.4.5', "Unrecognized IP V4 address: '1.2.3.4.5'"],
-			['192,168,0,25', "Unrecognized IP V4 address: '192,168,0,25'"],
-			['127. 0. 0. 1', "Unrecognized IP V4 address: '127. 0. 0. 1'"],
-			['-125.0.0.1', "Unrecognized IP V4 address: '-125.0.0.1'"],
-			['256.0.0.1', "Unrecognized IP V4 address: '256.0.0.1'"],
-			['0.-255.0.1', "Unrecognized IP V4 address: '0.-255.0.1'"],
-			['0.256.0.1', "Unrecognized IP V4 address: '0.256.0.1'"],
-			['0.0.-3.1', "Unrecognized IP V4 address: '0.0.-3.1'"],
-			['0.0.256.1', "Unrecognized IP V4 address: '0.0.256.1'"],
-			['0.0.0.-3', "Unrecognized IP V4 address: '0.0.0.-3'"],
-			['0.0.0.256', "Unrecognized IP V4 address: '0.0.0.256'"],
-			['1a.0.0.255', "Unrecognized IP V4 address: '1a.0.0.255'"],
-			['a1.0.0.255', "Unrecognized IP V4 address: 'a1.0.0.255'"],
-		];
-	}
+    /**
+     * @param string $fixture
+     * @param string $exceptionMessage
+     * @dataProvider correctlyFailsBuildingObjectDataProvider
+     */
+    public function testCorrectlyFailsBuildingObject(string $fixture, string $exceptionMessage): void {
+        $this->expectExceptionMessage($exceptionMessage);
+        $this->expectException(LogicException::class);
 
-	/**
-	 * @param string $fixture
-	 * @param string $exceptionMessage
-	 * @dataProvider correctlyFailsBuildingObjectDataProvider
-	 */
-	public function testCorrectlyFailsBuildingObject(string $fixture, string $exceptionMessage): void
-	{
-		$this->expectExceptionMessage($exceptionMessage);
-		$this->expectException(LogicException::class);
+        IpV4::constructFromString($fixture);
+    }
 
-		IpV4::constructFromString($fixture);
-	}
+    public function correctlyComparesIpV4DataProvider(): array {
+        return [
+            ['127.1.2.3', '127.1.2.3', true],
+            ['127.1.2.3', '127.255.2.3', false],
+            ['127.1.2.3', '127.1.255.3', false],
+            ['127.1.2.3', '127.1.2.255', false],
+            ['127.1.2.3', '3.1.2.3', false],
+        ];
+    }
+
+    /**
+     * @param string $ip1AsString
+     * @param string $ip2AsString
+     * @param bool $expected
+     * @return void
+     * @dataProvider correctlyComparesIpV4DataProvider
+     */
+    public function testCorrectlyComparesIpV4(string $ip1AsString, string $ip2AsString, bool $expected): void {
+        $ip1 = IpV4::constructFromString($ip1AsString);
+        $ip2 = IpV4::constructFromString($ip2AsString);
+
+        self::assertEquals($expected, $ip1->equalsTo($ip2));
+
+        // Idempotent
+        self::assertEquals($expected, $ip2->equalsTo($ip1));
+    }
 }
