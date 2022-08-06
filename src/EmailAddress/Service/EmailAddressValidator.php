3a15a1404503df061ebf0c4617837065d0d6efed
Nicky Santamaria <nicky.web.001@gmail.com>
2022-08-06 20:23:17 +0100

    Add static call

--- src/EmailAddress/Service/EmailAddressValidator.php
+++ src/EmailAddress/Service/EmailAddressValidator.php
@@ -8,7 +8,12 @@ namespace Utils\EmailAddress\Service;
 use Utils\EmailAddress\UseCase\ValidateEmailAddress;
 
 class EmailAddressValidator implements ValidateEmailAddress {
-    public function isValidEmailAddress(string $emailAddress): bool {
+
+    public static function isValidEmailAddressStatic(string $emailAddress): bool {
         return filter_var($emailAddress, FILTER_VALIDATE_EMAIL) !== false;
+    }
+
+    public function isValidEmailAddress(string $emailAddress): bool {
+        return self::isValidEmailAddressStatic($emailAddress);
     }
 }
