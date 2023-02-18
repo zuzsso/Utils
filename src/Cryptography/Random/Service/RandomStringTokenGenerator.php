3494e4de5e6dd6b32b08346d5ea75fd7e5fcaa93
Nicky Santamaria <nicky.web.001@gmail.com>
2023-02-18 18:53:05 +0000

    add a new way of generating hexadecimal random strings

--- src/Cryptography/Random/Service/RandomStringTokenGenerator.php
+++ src/Cryptography/Random/Service/RandomStringTokenGenerator.php
@@ -4,15 +4,18 @@ declare(strict_types=1);
 
 namespace Utils\Cryptography\Random\Service;
 
+use Exception;
 use InvalidArgumentException;
 use Throwable;
 use Utils\Cryptography\Random\Exception\UnableToGenerateRandomTokenGeneralException;
+use Utils\Cryptography\Random\Object\CharacterPool\AbstractCharacterPool;
 use Utils\Cryptography\Random\UseCase\GenerateRandomStringToken;
 
 class RandomStringTokenGenerator implements GenerateRandomStringToken {
 
     /**
      * @inheritDoc
+     * @deprecated use 'GenerateRandomStringToken::generateRandomStringOfLengthInChars()' method
      */
     public function hexTokenOfLength(int $charLength): string {
         if ($charLength < 1) {
@@ -25,5 +28,35 @@ class RandomStringTokenGenerator implements GenerateRandomStringToken {
         } catch (Throwable $e) {
             throw new UnableToGenerateRandomTokenGeneralException(null, null, $e);
         }
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function generateRandomStringOfLengthInChars(
+        AbstractCharacterPool $characterPool,
+        int $lengthInChars
+    ): string {
+        $poolSize = $characterPool->characterPoolSize();
+
+        if ($poolSize < 1) {
+            throw new InvalidArgumentException('Character pool not big enough');
+        }
+
+        if ($lengthInChars < 1) {
+            throw new InvalidArgumentException(
+                "The current implementation only generates random strings of length [1, " .
+                PHP_INT_MAX . "], but requested $lengthInChars"
+            );
+        }
+
+        $result = '';
+
+        while (strlen($result) < $lengthInChars) {
+            $atRandom = random_int(0, $poolSize - 1);
+            $result .= $characterPool->getCharAt($atRandom);
+        }
+
+        return $result;
     }
 }
