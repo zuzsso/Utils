12f85ce3043eb55e9280744f99356c07232a3937
Nicky Santamaria <nicky.web.001@gmail.com>
2023-02-18 18:51:40 +0000

    redefine this interface

--- src/Cryptography/Random/UseCase/GenerateRandomStringToken.php
+++ src/Cryptography/Random/UseCase/GenerateRandomStringToken.php
@@ -5,10 +5,18 @@ declare(strict_types=1);
 namespace Utils\Cryptography\Random\UseCase;
 
 use Utils\Cryptography\Random\Exception\UnableToGenerateRandomTokenGeneralException;
+use Utils\Cryptography\Random\Object\CharacterPool\AbstractCharacterPool;
 
 interface GenerateRandomStringToken {
     /**
      * @throws UnableToGenerateRandomTokenGeneralException
+     * @deprecated use 'GenerateRandomStringToken::generateRandomStringOfLengthInChars()' method, and pass an
+     * instance of HexadecimalLowerCaseCharacterPool as parameter
      */
     public function hexTokenOfLength(int $charLength): string;
+
+    public function generateRandomStringOfLengthInChars(
+        AbstractCharacterPool $characterPool,
+        int $lengthInChars
+    ): string;
 }
