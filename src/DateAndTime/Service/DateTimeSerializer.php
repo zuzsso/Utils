b78a068ab6776545b73fb12c5029f6df2ab169bf
Nicky Santamaria <nicky.web.001@gmail.com>
2023-07-08 19:40:05 +0100

    locale aware serializer and tests

--- src/DateAndTime/Service/DateTimeSerializer.php
+++ src/DateAndTime/Service/DateTimeSerializer.php
@@ -8,23 +8,89 @@ use DateTimeImmutable;
 use Utils\DateAndTime\UseCase\SerializeDateTime;
 
 class DateTimeSerializer implements SerializeDateTime {
+    private static array $longMonthNames = [
+        'spanish' => [
+            1 => 'Enero',
+            2 => 'Febrero',
+            3 => 'Marzo',
+            4 => 'Abril',
+            5 => 'Mayo',
+            6 => 'Junio',
+            7 => 'Julio',
+            8 => 'Agosto',
+            9 => 'Septiembre',
+            10 => 'Octubre',
+            11 => 'Noviembre',
+            12 => 'Diciembre',
+        ],
+        'french' => [
+            1 => 'janvier',
+            2 => 'février',
+            3 => 'mars',
+            4 => 'avril',
+            5 => 'mai',
+            6 => 'juin',
+            7 => 'juillet',
+            8 => 'août',
+            9 => 'septembre',
+            10 => 'octobre',
+            11 => 'novembre',
+            12 => 'décembre',
+        ],
+        'english' => [
+            1 => 'January',
+            2 => 'February',
+            3 => 'March',
+            4 => 'April',
+            5 => 'May',
+            6 => 'June',
+            7 => 'July',
+            8 => 'August',
+            9 => 'September',
+            10 => 'October',
+            11 => 'November',
+            12 => 'December',
+        ]
+    ];
+
     public function serializeImmutable(DateTimeImmutable $dateTime): array {
+        $monthNumeral = (int)$dateTime->format('m');
+        $monthLongNameFrench = self::$longMonthNames['french'][$monthNumeral];
+        $monthLongNameSpanish = self::$longMonthNames['spanish'][$monthNumeral];
+        $monthLongNameEnglish = self::$longMonthNames['english'][$monthNumeral];
+
+        $escapedLongMonthNameFrench = $this->escapeLiteral($monthLongNameFrench);
+        $escapedLongMonthNameSpanish = $this->escapeLiteral($monthLongNameSpanish);
+        $escapedLongMonthNameEnglish = $this->escapeLiteral($monthLongNameEnglish);
+
         return [
             'components' => [
                 'day' => (int)$dateTime->format('d'),
                 'month' => (int)$dateTime->format('m'),
                 'year' => (int)$dateTime->format('Y'),
                 'hour' => (int)$dateTime->format('H'),
-                'minute' => (int)$dateTime->format('m'),
+                'minute' => (int)$dateTime->format('i'),
                 'second' => (int)$dateTime->format('s')
             ],
             'formatted' => [
                 'longDate' => [
-                    'spanish' => $dateTime->format('j \d\e F, Y'),
-                    'english' => $dateTime->format('F jS, Y'),
-                    'french' => $dateTime->format('F j, Y'),
+                    'spanish' => $dateTime->format("j \\d\\e $escapedLongMonthNameSpanish, Y"),
+                    'english' => $dateTime->format("$escapedLongMonthNameEnglish jS, Y"),
+                    'french' => $dateTime->format("$escapedLongMonthNameFrench j, Y"),
                 ]
             ]
         ];
+    }
+
+    private function escapeLiteral(string $literal): string {
+        $result = '';
+
+        $len = strlen($literal);
+
+        for ($i = 0; $i < $len; $i++) {
+            $result .= '\\' . $literal[$i];
+        }
+
+        return $result;
     }
 }
---
+++ tests/DateAndTime/Service/DateTimeSerializerTest.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Utils\Tests\DateAndTime\Service;
+
+use DateTimeImmutable;
+use PHPUnit\Framework\TestCase;
+use Utils\DateAndTime\Service\DateTimeSerializer;
+
+class DateTimeSerializerTest extends TestCase {
+
+    private DateTimeSerializer $sut;
+
+    public function setUp(): void {
+        parent::setUp();
+        $this->sut = new DateTimeSerializer();
+    }
+
+    public function testCorrectlySerializes(): void {
+        $date = DateTimeImmutable::createFromFormat('Y-m-d H:i:s', '2023-07-08 18:35:45');
+        $actual = $this->sut->serializeImmutable($date);
+
+        $expected = [
+            'components' => [
+                'day' => 8,
+                'month' => 7,
+                'year' => 2023,
+                'hour' => 18,
+                'minute' => 35,
+                'second' => 45
+            ],
+            'formatted' => [
+                'longDate' => [
+                    'spanish' => '8 de Julio, 2023',
+                    'english' => 'July 8th, 2023',
+                    'french' => 'juillet 8, 2023'
+                ]
+            ]
+        ];
+
+        self::assertEquals($expected, $actual);
+    }
+}
