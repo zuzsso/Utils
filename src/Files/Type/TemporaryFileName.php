43adf1ef2762715882a3727b9b76d360144f16ae
Nicky <ndsans@gmail.com>
2023-09-14 20:56:42 +0100

    random file name generator

---
+++ src/Files/Type/TemporaryFileName.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Utils\Files\Type;
+
+use Utils\Cryptography\Random\Object\AbstractCrypToken;
+use Utils\Cryptography\Random\Object\CharacterPool\AbstractCharacterPool;
+use Utils\Cryptography\Random\Object\CharacterPool\AlphanumericCaseSensitive;
+
+class TemporaryFileName extends AbstractCrypToken {
+    public function getCharacterPool(): AbstractCharacterPool {
+        return new AlphanumericCaseSensitive();
+    }
+
+    public function getTokenLengthInOneByteChars(): int {
+        return 24;
+    }
+}
