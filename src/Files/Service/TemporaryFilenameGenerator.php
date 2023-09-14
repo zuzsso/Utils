af9dc424a3125c6849acf8801412c4471b17ee35
Nicky <ndsans@gmail.com>
2023-09-14 20:57:06 +0100

    service to generate temporary file names with extension

---
+++ src/Files/Service/TemporaryFilenameGenerator.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Utils\Files\Service;
+
+use Utils\Files\Exception\UnableToGenerateTemporaryFileException;
+use Utils\Files\Type\FileExtension\AbstractFileExtension;
+use Utils\Files\Type\TemporaryFileName;
+use Utils\Files\UseCase\CheckFileExists;
+use Utils\Files\UseCase\GenerateTemporaryFilename;
+
+class TemporaryFilenameGenerator implements GenerateTemporaryFilename {
+
+    private CheckFileExists $checkFileExists;
+
+    public function __construct(CheckFileExists $checkFileExists) {
+        $this->checkFileExists = $checkFileExists;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function randomNameWithExtensionAndGetFullPath(AbstractFileExtension $fileExtension): string {
+
+        $tmpFolder = sys_get_temp_dir();
+
+        $success = false;
+        $currentAttempt = 0;
+        $maxAttempts = 10;
+
+        do {
+
+            $tmpFileName = new TemporaryFileName();
+
+            $fullPath =
+                $tmpFolder .
+                '/' .
+                $tmpFileName->getCryptokenAsString() .
+                '.' .
+                $fileExtension::getExtensionLiteralNoDot();
+
+            if (!$this->checkFileExists->check($fullPath, false)) {
+                $success = true;
+            }
+
+            $currentAttempt++;
+        } while (!(($success === true) || ($currentAttempt > $maxAttempts)));
+
+        if ($success === false) {
+            throw new UnableToGenerateTemporaryFileException("Reached max attempts");
+        }
+
+        return $fullPath;
+    }
+}
---
+++ src/Files/UseCase/GenerateTemporaryFilename.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Utils\Files\UseCase;
+
+use Utils\Files\Exception\UnableToGenerateTemporaryFileException;
+use Utils\Files\Type\FileExtension\AbstractFileExtension;
+
+interface GenerateTemporaryFilename {
+    /**
+     * @throws UnableToGenerateTemporaryFileException
+     */
+    public function randomNameWithExtensionAndGetFullPath(AbstractFileExtension $fileExtension): string;
+}
