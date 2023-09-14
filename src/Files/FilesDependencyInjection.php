16ded6f089dda5afd4c503d08826a33bd9c21b53
Nicky <ndsans@gmail.com>
2023-09-14 20:57:17 +0100

    dependency injection update

--- src/Files/FilesDependencyInjection.php
+++ src/Files/FilesDependencyInjection.php
@@ -9,10 +9,12 @@ use Utils\Files\Service\DirectoryIsReadableChecker;
 use Utils\Files\Service\DirectoryIsWriteableChecker;
 use Utils\Files\Service\FileExistsChecker;
 use Utils\Files\Service\FolderExistsEnsurer;
+use Utils\Files\Service\TemporaryFilenameGenerator;
 use Utils\Files\UseCase\CheckDirectoryIsReadable;
 use Utils\Files\UseCase\CheckDirectoryIsWriteable;
 use Utils\Files\UseCase\CheckFileExists;
 use Utils\Files\UseCase\EnsureFolderExists;
+use Utils\Files\UseCase\GenerateTemporaryFilename;
 use function DI\autowire;
 
 class FilesDependencyInjection extends AbstractDependencyInjection {
@@ -22,6 +24,7 @@ class FilesDependencyInjection extends AbstractDependencyInjection {
             CheckDirectoryIsWriteable::class => autowire(DirectoryIsWriteableChecker::class),
             CheckFileExists::class => autowire(FileExistsChecker::class),
             EnsureFolderExists::class => autowire(FolderExistsEnsurer::class),
+            GenerateTemporaryFilename::class => autowire(TemporaryFilenameGenerator::class)
         ];
     }
 }
