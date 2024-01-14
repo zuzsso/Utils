a871f67f7c2bd4ffffb7dbb3cf6517a5b4846df8
Nicky Santamaria <ndsans@gmail.com>
2024-01-14 18:55:08 +0000

    Serialize date to API depending on the language instead

--- src/DateAndTime/Service/DateTimeSerializer.php
+++ src/DateAndTime/Service/DateTimeSerializer.php
@@ -6,83 +6,134 @@ namespace Utils\DateAndTime\Service;
 
 use DateTimeImmutable;
 use Utils\DateAndTime\UseCase\SerializeDateTime;
+use Utils\Language\Type\Language\AbstractLanguage;
+use Utils\Language\Type\Language\English;
+use Utils\Language\Type\Language\French;
+use Utils\Language\Type\Language\Spanish;
 
-class DateTimeSerializer implements SerializeDateTime {
-    private static array $longMonthNames = [
-        'spanish' => [
-            1 => 'Enero',
-            2 => 'Febrero',
-            3 => 'Marzo',
-            4 => 'Abril',
-            5 => 'Mayo',
-            6 => 'Junio',
-            7 => 'Julio',
-            8 => 'Agosto',
-            9 => 'Septiembre',
-            10 => 'Octubre',
-            11 => 'Noviembre',
-            12 => 'Diciembre',
-        ],
-        'french' => [
-            1 => 'janvier',
-            2 => 'février',
-            3 => 'mars',
-            4 => 'avril',
-            5 => 'mai',
-            6 => 'juin',
-            7 => 'juillet',
-            8 => 'août',
-            9 => 'septembre',
-            10 => 'octobre',
-            11 => 'novembre',
-            12 => 'décembre',
-        ],
-        'english' => [
-            1 => 'January',
-            2 => 'February',
-            3 => 'March',
-            4 => 'April',
-            5 => 'May',
-            6 => 'June',
-            7 => 'July',
-            8 => 'August',
-            9 => 'September',
-            10 => 'October',
-            11 => 'November',
-            12 => 'December',
-        ]
-    ];
+class DateTimeSerializer implements SerializeDateTime
+{
+    private function getLongMonthNames(): array
+    {
+        return [
+            Spanish::getApiLiteral() => [
+                'Enero',
+                'Febrero',
+                'Marzo',
+                'Abril',
+                'Mayo',
+                'Junio',
+                'Julio',
+                'Agosto',
+                'Septiembre',
+                'Octubre',
+                'Noviembre',
+                'Diciembre',
+            ],
+            French::getApiLiteral() => [
+                'janvier',
+                'février',
+                'mars',
+                'avril',
+                'mai',
+                'juin',
+                'juillet',
+                'août',
+                'septembre',
+                'octobre',
+                'novembre',
+                'décembre',
+            ],
+            English::getApiLiteral() => [
+                'January',
+                'February',
+                'March',
+                'April',
+                'May',
+                'June',
+                'July',
+                'August',
+                'September',
+                'October',
+                'November',
+                'December',
+            ],
+        ];
+    }
 
-    public function serializeImmutable(DateTimeImmutable $dateTime): array {
-        $monthNumeral = (int)$dateTime->format('m');
-        $monthLongNameFrench = self::$longMonthNames['french'][$monthNumeral];
-        $monthLongNameSpanish = self::$longMonthNames['spanish'][$monthNumeral];
-        $monthLongNameEnglish = self::$longMonthNames['english'][$monthNumeral];
+    private function serializeComponents(DateTimeImmutable $dateTime): array
+    {
+        return [
+            'day' => (int)$dateTime->format('d'),
+            'month' => (int)$dateTime->format('m'),
+            'year' => (int)$dateTime->format('Y'),
+            'hour' => (int)$dateTime->format('H'),
+            'minute' => (int)$dateTime->format('i'),
+            'second' => (int)$dateTime->format('s'),
+        ];
+    }
+
+    /**
+     * @deprecated
+     * @see \Utils\DateAndTime\Service\DateTimeSerializer::serializeImmutableForLanguage
+     */
+    public function serializeImmutable(DateTimeImmutable $dateTime): array
+    {
+        $monthNumeralZeroBased = (int)$dateTime->format('m') - 1;
+
+        $longMonthNames = $this->getLongMonthNames();
+
+
+        $monthLongNameFrench = $longMonthNames[French::getApiLiteral()][$monthNumeralZeroBased];
+        $monthLongNameSpanish = $longMonthNames[Spanish::getApiLiteral()][$monthNumeralZeroBased];
+        $monthLongNameEnglish = $longMonthNames[English::getApiLiteral()][$monthNumeralZeroBased];
 
         $escapedLongMonthNameFrench = $this->escapeLiteral($monthLongNameFrench);
         $escapedLongMonthNameSpanish = $this->escapeLiteral($monthLongNameSpanish);
         $escapedLongMonthNameEnglish = $this->escapeLiteral($monthLongNameEnglish);
 
         return [
-            'components' => [
-                'day' => (int)$dateTime->format('d'),
-                'month' => (int)$dateTime->format('m'),
-                'year' => (int)$dateTime->format('Y'),
-                'hour' => (int)$dateTime->format('H'),
-                'minute' => (int)$dateTime->format('i'),
-                'second' => (int)$dateTime->format('s')
-            ],
+            'components' => $this->serializeComponents($dateTime),
             'formatted' => [
                 'longDate' => [
                     'spanish' => $dateTime->format("j \\d\\e $escapedLongMonthNameSpanish, Y"),
                     'english' => $dateTime->format("$escapedLongMonthNameEnglish jS, Y"),
                     'french' => $dateTime->format("$escapedLongMonthNameFrench j, Y"),
-                ]
-            ]
+                ],
+            ],
         ];
     }
 
-    private function escapeLiteral(string $literal): string {
+    public function serializeImmutableForLanguage(DateTimeImmutable $dateTime, AbstractLanguage $lan): array
+    {
+        $monthNumeralZeroBased = (int)$dateTime->format('m');
+
+        $longMonthNames = $this->getLongMonthNames();
+
+        if ($lan instanceof French) {
+            $monthLongNameFrench = $longMonthNames[French::getApiLiteral()][$monthNumeralZeroBased];
+            $escapedLongMonthNameFrench = $this->escapeLiteral($monthLongNameFrench);
+            $long = $dateTime->format("$escapedLongMonthNameFrench j, Y");
+        } elseif ($lan instanceof Spanish) {
+            $monthLongNameSpanish = $longMonthNames[Spanish::getApiLiteral()][$monthNumeralZeroBased];
+            $escapedLongMonthNameSpanish = $this->escapeLiteral($monthLongNameSpanish);
+            $long = $dateTime->format("j \\d\\e $escapedLongMonthNameSpanish, Y");
+        } else {
+            $monthLongName = $longMonthNames[$lan::getApiLiteral()][$monthNumeralZeroBased];
+            $escapedLongMonthName = $this->escapeLiteral($monthLongName);
+            $long = $dateTime->format("$escapedLongMonthName jS, Y");
+        }
+
+        return [
+            'components' => $this->serializeComponents($dateTime),
+            'formatted' => [
+                'longDate' => $long,
+            ],
+        ];
+    }
+
+    private function escapeLiteral(string $literal): string
+    {
         $result = '';
 
         $len = strlen($literal);
--- src/DateAndTime/UseCase/SerializeDateTime.php
+++ src/DateAndTime/UseCase/SerializeDateTime.php
@@ -5,7 +5,14 @@ declare(strict_types=1);
 namespace Utils\DateAndTime\UseCase;
 
 use DateTimeImmutable;
+use Utils\Language\Type\Language\AbstractLanguage;
 
 interface SerializeDateTime {
+    /**
+     * @deprecated
+     * @see \Utils\DateAndTime\UseCase\SerializeDateTime::serializeImmutableForLanguage
+     */
     public function serializeImmutable(DateTimeImmutable $dateTime): array;
+
+    public function serializeImmutableForLanguage(DateTimeImmutable $dateTime, AbstractLanguage $lan): array;
 }
