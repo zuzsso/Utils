07b4ffcb8fa66cb518015cedfbe536415d2d87f4
Nicky Santamaria <ndsans@gmail.com>
2024-02-04 19:11:58 +0000

    refactoring

--- src/DateAndTime/Service/DateTimeTransformer.php
+++ src/DateAndTime/Service/DateTimeTransformer.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace Utils\DateAndTime\Service;
 
+use DateInterval;
 use DateTimeImmutable;
+use Utils\DateAndTime\Exception\DatetimeCommonOperationsUnmanagedException;
 use Utils\DateAndTime\UseCase\TransformDateTime;
 
 class DateTimeTransformer implements TransformDateTime
@@ -12,5 +14,16 @@ class DateTimeTransformer implements TransformDateTime
     public function removeTime(DateTimeImmutable $dateTimeImmutable): DateTimeImmutable
     {
         return $dateTimeImmutable->modify('midnight');
+    }
+
+    public function substractDays(DateTimeImmutable $d, int $days): DateTimeImmutable
+    {
+        if ($days <= 0) {
+            throw new DatetimeCommonOperationsUnmanagedException(
+                "This function requires a positive number of days and greater than 0 to substract. Provided: $days"
+            );
+        }
+
+        return $d->sub(new DateInterval("P${days}D"));
     }
 }
--- src/DateAndTime/UseCase/TransformDateTime.php
+++ src/DateAndTime/UseCase/TransformDateTime.php
@@ -5,8 +5,14 @@ declare(strict_types=1);
 namespace Utils\DateAndTime\UseCase;
 
 use DateTimeImmutable;
+use Utils\DateAndTime\Exception\DatetimeCommonOperationsUnmanagedException;
 
 interface TransformDateTime
 {
     public function removeTime(DateTimeImmutable $dateTimeImmutable): DateTimeImmutable;
+
+    /**
+     * @throws DatetimeCommonOperationsUnmanagedException
+     */
+    public function substractDays(DateTimeImmutable $d, int $days): DateTimeImmutable;
 }
