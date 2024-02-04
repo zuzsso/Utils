c3e244786334da5e7a6eb8ceef9f09061eea4427
Nicky Santamaria <ndsans@gmail.com>
2024-02-04 10:22:01 +0000

    service to implement common date time operations

--- src/DateAndTime/DateAndTimeDependencyInjection.php
+++ src/DateAndTime/DateAndTimeDependencyInjection.php
@@ -5,9 +5,11 @@ declare(strict_types=1);
 namespace Utils\DateAndTime;
 
 use Utils\AbstractDependencyInjection;
+use Utils\DateAndTime\Service\DateTimeCommonOperationsImplementer;
 use Utils\DateAndTime\Service\DateTimeProvider;
 use Utils\DateAndTime\Service\DateTimeSerializer;
 use Utils\DateAndTime\Service\DateTimeTransformer;
+use Utils\DateAndTime\UseCase\ImplementDateTimeCommonOperations;
 use Utils\DateAndTime\UseCase\ProvideDateTime;
 use Utils\DateAndTime\UseCase\SerializeDateTime;
 use Utils\DateAndTime\UseCase\TransformDateTime;
@@ -16,6 +18,7 @@ use function DI\autowire;
 class DateAndTimeDependencyInjection extends AbstractDependencyInjection {
     public static function getDependencies(): array {
         return [
+            ImplementDateTimeCommonOperations::class => autowire(DateTimeCommonOperationsImplementer::class),
             ProvideDateTime::class => autowire(DateTimeProvider::class),
             TransformDateTime::class => autowire(DateTimeTransformer::class),
             SerializeDateTime::class => autowire(DateTimeSerializer::class),
---
+++ src/DateAndTime/Service/DateTimeCommonOperationsImplementer.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Utils\DateAndTime\Service;
+
+use Utils\DateAndTime\UseCase\ImplementDateTimeCommonOperations;
+
+class DateTimeCommonOperationsImplementer implements ImplementDateTimeCommonOperations
+{
+
+}
