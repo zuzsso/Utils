e68055356a22e36ae0083c9b63bb85c4c48cdb26
Nicky Santamaria <ndsans@gmail.com>
2024-02-04 10:27:44 +0000

    implement this operation

--- src/DateAndTime/Service/DateTimeCommonOperationsImplementer.php
+++ src/DateAndTime/Service/DateTimeCommonOperationsImplementer.php
@@ -4,9 +4,27 @@ declare(strict_types=1);
 
 namespace Utils\DateAndTime\Service;
 
+use Utils\DateAndTime\Exception\DatetimeCommonOperationsUnmanagedException;
 use Utils\DateAndTime\UseCase\ImplementDateTimeCommonOperations;
+use DateTimeImmutable;
 
 class DateTimeCommonOperationsImplementer implements ImplementDateTimeCommonOperations
 {
+    private const MYSQL_DATE_TIME_FORMAT = 'Y-m-d H:i:s';
 
+    /**
+     * @inheritDoc
+     */
+    public function dateTimeImmutableFromMySql(string $mysqlDateTime): DateTimeImmutable
+    {
+        $result = DateTimeImmutable::createFromFormat(self::MYSQL_DATE_TIME_FORMAT, $mysqlDateTime);
+
+        if ($result === false) {
+            throw new DatetimeCommonOperationsUnmanagedException(
+                "Could not convert literal '$mysqlDateTime' to Date time format " . self::MYSQL_DATE_TIME_FORMAT
+            );
+        }
+
+        return $result;
+    }
 }
--- src/DateAndTime/UseCase/ImplementDateTimeCommonOperations.php
+++ src/DateAndTime/UseCase/ImplementDateTimeCommonOperations.php
@@ -4,7 +4,13 @@ declare(strict_types=1);
 
 namespace Utils\DateAndTime\UseCase;
 
+use DateTimeImmutable;
+use Utils\DateAndTime\Exception\DatetimeCommonOperationsUnmanagedException;
+
 interface ImplementDateTimeCommonOperations
 {
-
+    /**
+     * @throws DatetimeCommonOperationsUnmanagedException
+     */
+    public function dateTimeImmutableFromMySql(string $mysqlDateTime): DateTimeImmutable;
 }
