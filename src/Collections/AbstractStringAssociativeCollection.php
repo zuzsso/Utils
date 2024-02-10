c919e11e8bb89cec392d05dfc19ccca483bb63c1
Nicky Santamaria <ndsans@gmail.com>
2024-02-10 22:47:24 +0000

    further improvements in this collection

--- src/Collections/AbstractStringAssociativeCollection.php
+++ src/Collections/AbstractStringAssociativeCollection.php
@@ -18,34 +18,15 @@ abstract class AbstractStringAssociativeCollection implements Iterator, Countabl
 {
     /**
      * @deprecated This class parameter will be made private in future releases
-     * @see \Utils\Collections\AbstractStringAssociativeCollection::addStringKeyUntyped
+     * @see        \Utils\Collections\AbstractStringAssociativeCollection::addStringKeyUntyped
      */
     protected array $collection = [];
 
     private int $it = 0;
 
-    /**
-     * @throws KeyAlreadyExistsException
-     */
-    final protected function addStringKeyUntyped(string $key, $value): void
+    final protected function addUnindexedUntypedElement($value): void
     {
-        if (array_key_exists($key, $this->collection)) {
-            throw new KeyAlreadyExistsException("Key '$key' already exists in this collection");
-        }
-
-        $this->collection[$key] = $value;
-    }
-
-    /**
-     * @throws KeyAlreadyExistsException
-     */
-    final protected function addIntKeyUntyped(?int $key, $value): void
-    {
-        if ($key !== null) {
-            $this->addStringKeyUntyped((string)$key, $value);
-        } else {
-            $this->collection[] = $value;
-        }
+        $this->collection[] = $value;
     }
 
     /** @deprecated Not to be manipulated by child classes */
@@ -66,8 +47,7 @@ abstract class AbstractStringAssociativeCollection implements Iterator, Countabl
     final public function valid(): bool
     {
         return
-            ($this->it >= 0) &&
-            ($this->it < count($this->collection));
+            ($this->it >= 0) && ($this->it < count($this->collection));
     }
 
     /** @deprecated Not to be manipulated by child classes */
@@ -80,6 +60,41 @@ abstract class AbstractStringAssociativeCollection implements Iterator, Countabl
     final public function count(): int
     {
         return count($this->collection);
+    }
+
+    /**
+     * @throws ArrayIndexOutOfBoundsException
+     */
+    abstract public function getByStringKey(string $key);
+
+    /**
+     * @throws ArrayIndexOutOfBoundsException
+     */
+    abstract public function getByNumericOffset(int $offset);
+
+    /**
+     * @throws KeyAlreadyExistsException
+     * @deprecated For unindexed, see "addUnindexedUntypedElement". this value will become not nullable in future releases
+     */
+    final protected function addIntKeyUntyped(?int $key, $value): void
+    {
+        if ($key !== null) {
+            $this->addStringKeyUntyped((string)$key, $value);
+        } else {
+            $this->addUnindexedUntypedElement($value);
+        }
+    }
+
+    /**
+     * @throws KeyAlreadyExistsException
+     */
+    final protected function addStringKeyUntyped(string $key, $value): void
+    {
+        if (array_key_exists($key, $this->collection)) {
+            throw new KeyAlreadyExistsException("Key '$key' already exists in this collection");
+        }
+
+        $this->collection[$key] = $value;
     }
 
     /**
@@ -100,11 +115,6 @@ abstract class AbstractStringAssociativeCollection implements Iterator, Countabl
     /**
      * @throws ArrayIndexOutOfBoundsException
      */
-    abstract public function getByStringKey(string $key);
-
-    /**
-     * @throws ArrayIndexOutOfBoundsException
-     */
     final protected function getByStringKeyUntyped(string $key)
     {
         if (!array_key_exists($key, $this->collection)) {
@@ -113,11 +123,6 @@ abstract class AbstractStringAssociativeCollection implements Iterator, Countabl
 
         return $this->collection[$key];
     }
-
-    /**
-     * @throws ArrayIndexOutOfBoundsException
-     */
-    abstract public function getByNumericOffset(int $offset);
 
     /**
      * @throws ArrayIndexOutOfBoundsException
