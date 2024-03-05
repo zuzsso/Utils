60531ffae9745ba316963f4e2289d7f86c5496df
Nicky Santamaria <ndsans@gmail.com>
2024-03-05 21:21:01 +0000

    clarification

--- src/Files/UseCase/CheckFileExists.php
+++ src/Files/UseCase/CheckFileExists.php
@@ -5,9 +5,9 @@ namespace Utils\Files\UseCase;
 interface CheckFileExists
 {
     /**
-     * @param string $path
-     * @param bool $checkEmpty
-     * @return bool
+     * Checks whether the file exists or not
+     *
+     * If $checkEmpty is set to 'true', then the function will return 'false' even if the file exists but has no content
      */
     public function check(string $path, bool $checkEmpty): bool;
 }
