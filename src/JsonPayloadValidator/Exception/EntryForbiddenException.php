0868f7280cfd3525974feb65b5b4ae501136fe07
Nicky Santamaria <ndsans@gmail.com>
2024-03-16 19:06:27 +0000

    recondition these exceptions

--- src/JsonPayloadValidator/Exception/EntryForbiddenException.php
+++ src/JsonPayloadValidator/Exception/EntryForbiddenException.php
@@ -8,10 +8,7 @@ class EntryForbiddenException extends AbstractMalformedRequestBody
 {
     public static function constructForKeyNameForbidden(string $key): self
     {
-        return new self(
-            "Entry '%key%' should not be present in the payload",
-            ['key' => $key]
-        );
+        return new self("Entry '$key' should not be present in the payload");
     }
 
     public function errorCode(): string
