4b312cd4dd5d4861df48540370efd10443eb070d
Nicky Santamaria <ndsans@gmail.com>
2024-03-18 21:21:32 +0000

    unit testing

---
+++ tests/JsonPayloadValidator/PropertyBooleanCheckerTest.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Utils\Tests\JsonPayloadValidator;
+
+use PHPUnit\Framework\TestCase;
+use Utils\JsonPayloadValidator\Exception\EntryEmptyException;
+use Utils\JsonPayloadValidator\Exception\EntryMissingException;
+use Utils\JsonPayloadValidator\Exception\InvalidBoolValueException;
+use Utils\JsonPayloadValidator\Service\PropertyBooleanChecker;
+use Utils\JsonPayloadValidator\Service\PropertyPresenceChecker;
+
+class PropertyBooleanCheckerTest extends TestCase
+{
+    private PropertyBooleanChecker $sut;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->sut = new PropertyBooleanChecker(new PropertyPresenceChecker());
+    }
+
+    public function shouldFailRequiredDataProvider(): array
+    {
+        $key = 'myKey';
+
+        $m1 = "Entry 'myKey' missing";
+        $m2 = "Entry 'myKey' empty";
+        $m3 = "The entry 'myKey' does not hold a valid boolean value";
+
+        return [
+            [$key, [], EntryMissingException::class, $m1],
+            [$key, [$key => null], EntryEmptyException::class, $m2],
+            [$key, [$key => ''], EntryEmptyException::class, $m2],
+            [$key, [$key => '   '], EntryEmptyException::class, $m2],
+            [$key, [$key => []], EntryEmptyException::class, $m2],
+            [$key, [$key => 0], InvalidBoolValueException::class, $m3],
+            [$key, [$key => "blah"], InvalidBoolValueException::class, $m3],
+            [$key, [$key => 1], InvalidBoolValueException::class, $m3],
+            [$key, [$key => "0"], InvalidBoolValueException::class, $m3],
+            [$key, [$key => "1"], InvalidBoolValueException::class, $m3],
+            [$key, [$key => [[]]], InvalidBoolValueException::class, $m3],
+        ];
+    }
+
+    /**
+     * @dataProvider shouldFailRequiredDataProvider
+     * @throws EntryEmptyException
+     * @throws EntryMissingException
+     * @throws InvalidBoolValueException
+     */
+    public function testShouldFailRequired(
+        string $key,
+        array $payload,
+        string $expectedException,
+        string $expectedExceptionMessage
+    ): void {
+        $this->expectException($expectedException);
+        $this->expectExceptionMessage($expectedExceptionMessage);
+
+        $this->sut->required($key, $payload);
+
+        $this->expectNotToPerformAssertions();
+    }
+}
