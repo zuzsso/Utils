a89be431bf739aaa53594134ee9257fe7260fc96
Nicky Santamaria <ndsans@gmail.com>
2024-03-19 21:51:36 +0000

    unit testing

--- src/JsonPayloadValidator/UseCase/CheckPropertyArray.php
+++ src/JsonPayloadValidator/UseCase/CheckPropertyArray.php
@@ -6,14 +6,11 @@ namespace Utils\JsonPayloadValidator\UseCase;
 
 use Utils\JsonPayloadValidator\Exception\ArrayDoesNotHaveMinimumElementCountException;
 use Utils\JsonPayloadValidator\Exception\ArrayExceedsMaximumnAllowedNumberOfElementsException;
-use Utils\JsonPayloadValidator\Exception\ArrayWithCustomIndexNumeration;
 use Utils\JsonPayloadValidator\Exception\ValueNotAJsonObjectException;
 use Utils\JsonPayloadValidator\Exception\EntryEmptyException;
 use Utils\JsonPayloadValidator\Exception\EntryMissingException;
-use Utils\JsonPayloadValidator\Exception\NotNumericArrayIndexException;
 use Utils\JsonPayloadValidator\Exception\RequiredArrayIsEmptyException;
 use Utils\JsonPayloadValidator\Exception\ValueNotAnArrayException;
-use Utils\JsonPayloadValidator\Service\PropertyArrayChecker;
 
 interface CheckPropertyArray
 {
@@ -27,9 +24,10 @@ interface CheckPropertyArray
     public function optionalKey(string $key, array $payload): self;
 
     /**
-     * @throws ArrayWithCustomIndexNumeration
+     * @throws EntryEmptyException
+     * @throws EntryMissingException
+     * @throws RequiredArrayIsEmptyException
      * @throws ValueNotAJsonObjectException
-     * @throws NotNumericArrayIndexException
      * @throws ValueNotAnArrayException
      */
     public function keyOfJsonObjects(string $key, array $payload, bool $required = true): self;
