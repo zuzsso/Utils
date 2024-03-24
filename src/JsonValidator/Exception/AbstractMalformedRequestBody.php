e68fec891d611ce3cbcf2cd6947e2e176830b3ab
Nicky Santamaria <ndsans@gmail.com>
2024-03-24 18:57:27 +0000

    serializer

--- src/JsonValidator/Exception/AbstractMalformedRequestBody.php
+++ src/JsonValidator/Exception/AbstractMalformedRequestBody.php
@@ -8,4 +8,11 @@ use Exception;
 
 abstract class AbstractMalformedRequestBody extends Exception
 {
+    final public function serialize(): array
+    {
+        return [
+            'code' => $this->getCode(),
+            'message' => $this->getMessage()
+        ];
+    }
 }
