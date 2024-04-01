17ed1b7869b892f4237545a15f79d14b7997f0a2
Nicky Santamaria <ndsans@gmail.com>
2024-04-01 14:45:40 +0100

    new format

--- src/JsonValidator/JsonPayloadValidatorDependencyInjection.php
+++ src/JsonValidator/JsonPayloadValidatorDependencyInjection.php
@@ -10,6 +10,7 @@ use Utils\JsonValidator\Service\KeyBooleanChecker;
 use Utils\JsonValidator\Service\KeyEnumChecker;
 use Utils\JsonValidator\Service\KeyFloatChecker;
 use Utils\JsonValidator\Service\KeyIntegerChecker;
+use Utils\JsonValidator\Service\KeyJsonObjectChecker;
 use Utils\JsonValidator\Service\KeyPresenceChecker;
 use Utils\JsonValidator\Service\KeyStringChecker;
 use Utils\JsonValidator\Service\ValueArrayChecker;
@@ -20,6 +21,7 @@ use Utils\JsonValidator\UseCase\CheckKeyBoolean;
 use Utils\JsonValidator\UseCase\CheckKeyEnum;
 use Utils\JsonValidator\UseCase\CheckKeyFloat;
 use Utils\JsonValidator\UseCase\CheckKeyInteger;
+use Utils\JsonValidator\UseCase\CheckKeyJsonObject;
 use Utils\JsonValidator\UseCase\CheckKeyPresence;
 use Utils\JsonValidator\UseCase\CheckKeyString;
 use Utils\JsonValidator\UseCase\CheckValueArray;
@@ -34,6 +36,7 @@ class JsonPayloadValidatorDependencyInjection extends AbstractDependencyInjectio
     public static function getDependencies(): array
     {
         return [
+            CheckKeyJsonObject::class => autowire(KeyJsonObjectChecker::class),
             CheckKeyArray::class => autowire(KeyArrayChecker::class),
             CheckKeyEnum::class => autowire(KeyEnumChecker::class),
             CheckKeyFloat::class => autowire(KeyFloatChecker::class),
