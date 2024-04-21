270a33f6dd43de2d1505f45ebdc248c3ff774846
Nicky Santamaria <ndsans@gmail.com>
2024-04-21 19:16:43 +0100

    commit

--- src/JsonValidator/Exception/ValueArrayNotExactLengthException.php
+++ src/JsonValidator/Exception/ValueArrayNotExactLengthException.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 
 namespace Utils\JsonValidator\Exception;
 
+/**
+ * @deprecated
+ * Migrated to zuzsso/json-validator
+ */
 class ValueArrayNotExactLengthException extends AbstractMalformedRequestBody
 {
     /**
